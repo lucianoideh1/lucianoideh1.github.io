54711e4320eab49a8cd5547cd2448822ae6ac49b
Luciano Vazquez <vazquezluciano5@gmail.com>
2024-11-11 13:31:21 -0300

    add download feature

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ mobileBtn.addEventListener('click', function() {
     console.log("toggle clicked")
     document.body.classList.toggle('dark-theme')
 })
+
+document.getElementById('download-btn').addEventListener('click', function () {
+    const link = document.createElement('a');
+    link.href = './resume.pdf'; 
+    link.download = 'Esteban-Vazquez-CV.pdf'; // Suggested filename
+    link.click(); // Trigger the download
+  });
