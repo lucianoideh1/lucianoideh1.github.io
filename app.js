eaa0ca6d301aac66b4fa1c05a2b20fc9b898a892
Luciano Vazquez <vazquezluciano5@gmail.com>
2025-02-10 18:36:17 -0300

    portfolio 2025

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const btn = document.querySelector('.btn-toggle')
-btn.addEventListener('click', function() {
-    console.log("toggle clicked")
-    document.body.classList.toggle('dark-theme')
-})
 
-const mobileBtn = document.querySelector('.mobile-btn-toggle')
-mobileBtn.addEventListener('click', function() {
-    console.log("toggle clicked")
-    document.body.classList.toggle('dark-theme')
-})
 
 document.getElementById('download-btn').addEventListener('click', function () {
     const link = document.createElement('a');
     link.href = './resume.pdf'; 
-    link.download = 'Esteban-Vazquez-CV.pdf'; // Suggested filename
+    link.download = 'Esteban-Vazquez Full stack developer.pdf'; // Suggested filename
     link.click(); // Trigger the download
   });
